7d076ca74654236741c4d05f2784229e74fa801a
MUKILTEO\1709575 <noahb2675@gmail.com>
2021-11-29 10:22:08 -0800

    started enemy ai

---
+++ Assets/Noah/Scripts/EnemyAI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class EnemyAI : MonoBehaviour
+{
+    //Reference to waypoints
+    public List<Transform> points;
+    //The int value for the next point index
+    public int nextID;
+    //The value of the that applies to ID for changing
+    int idChangeValue = 1;
+
+    private void Reset()
+    {
+        Init();
+    }
+
+    void Init()
+    {
+        //Make box collider trigger
+        GetComponent<BoxCollider2D>().isTrigger = true;
+
+        //Create Root object
+        GameObject root = new GameObject(name + "Root");
+        //Reset Position of Root to enemy object
+        root.transform.position = transform.position;
+        //Set enemy object as child of root
+        transform.SetParent(root.transform);
+        //Create Waypoints object
+        GameObject waypoints = new GameObject("Waypoints");
+        //Reset waypoints position to enemy to root
+        //Make waypoints object child of root
+        waypoints.transform.SetParent(root.transform);
+        waypoints.transform.position = root.transform.position;
+        //Create two point (gameobject) and reset their position to waypoints objects
+        //Make the points children of waypoint object
+        GameObject p1 = new GameObject("Point1"); p1.transform.SetParent(waypoints.transform);p1.transform.position = root.transform.position;
+        GameObject p2 = new GameObject("Point2"); p2.transform.SetParent(waypoints.transform);p2.transform.position = root.transform.position;
+
+        //Init points list then add the points to it
+        points = new List<Transform>();
+        points.Add(p1.transform);
+        points.Add(p2.transform);
+    }
+    private void Update()
+    {
+        MoveToNextPoint();
+    }
+
+    void MoveToNextPoint()
+    {
+
+    }
+}
