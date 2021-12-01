efaaeb591922dde4b67c8b349fb515a83225b388
MUKILTEO\1711757 <1711757@mukilteo.wednet.edu>
2021-12-01 08:29:09 -0800

    Watching a tutorial

---
+++ Assets/PlayerCombat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Attack();
+        }
+    }
+    void Attack()
+    {
+        //Play an attack animation
+        //Detect enemies in range of attack
+        // Damage them
+    }
+}
