4d858204de276cf620e0be71f5bdeef844774c58
MUKILTEO\1710194 <1710194@mukilteo.wednet.edu>
2021-12-08 10:16:44 -0800

    player movement done hell yeah

--- Assets/Tyler/Scripts/PlayerMovement.cs
+++ Assets/Tyler/Scripts/PlayerMovement.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Movement and Jump Variables
     private Rigidbody2D rb;
-    SpriteRenderer sr;
 
     private bool CanMove = true;
-    private bool CanJump = true;
+    private bool CanJump;
     private bool OnGround;
 
     [SerializeField] private float MovementSpeed;
@@ -28,6 +28,7 @@ public class PlayerMovement : MonoBehaviour
 
     RaycastHit2D[][] AllRaycastHits = new RaycastHit2D[3][];
 
+    //Wall Jump Variables
     private bool OnWallRight;
     private bool OnWallLeft;
 
@@ -36,7 +37,6 @@ public class PlayerMovement : MonoBehaviour
 
     RaycastHit2D[] WallHitsLeft;
     RaycastHit2D[] WallHitsRight;
-
 
     private void Awake()
     {
@@ -49,30 +49,54 @@ public class PlayerMovement : MonoBehaviour
         Jump();
     }
 
+    private void Movement()
+    {
+        if (CanMove)
+        {
+            if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                rb.velocity = new Vector2(MovementSpeed * Time.fixedDeltaTime, rb.velocity.y);
+            }
+            else if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                rb.velocity = new Vector2(-MovementSpeed * Time.fixedDeltaTime, rb.velocity.y);
+            }
+            else
+            {
+                rb.velocity = new Vector2(0, rb.velocity.y);
+            }
+        }
+    }
+
     private void RaySetup()
     {
-         RayPositionCenter = transform.position + new Vector3(0, RayLength * 1f, 0);
-         RayPositionLeft = transform.position + new Vector3(-RayPositionOffset, RayLength * 1f, 0);
-         RayPositionRight = transform.position + new Vector3(RayPositionOffset, RayLength * 1f, 0);
+        //Ground Check/Standard Jump Ray Setup
+        RayPositionCenter = transform.position + new Vector3(0, RayLength * -.5f, 0);
+        RayPositionLeft = transform.position + new Vector3(-RayPositionOffset, RayLength * -.5f, 0);
+        RayPositionRight = transform.position + new Vector3(RayPositionOffset, RayLength * -.5f, 0);
 
-         GroundHitsCenter = Physics2D.RaycastAll(RayPositionCenter, -Vector2.up, RayLength);
-         GroundHitsLeft = Physics2D.RaycastAll(RayPositionLeft, -Vector2.up, RayLength);
-         GroundHitsRight = Physics2D.RaycastAll(RayPositionRight, -Vector2.up, RayLength);
+        GroundHitsCenter = Physics2D.RaycastAll(RayPositionCenter, -Vector2.up, RayLength);
+        GroundHitsLeft = Physics2D.RaycastAll(RayPositionLeft, -Vector2.up, RayLength);
+        GroundHitsRight = Physics2D.RaycastAll(RayPositionRight, -Vector2.up, RayLength);
 
-         AllRaycastHits[0] = GroundHitsCenter;
-         AllRaycastHits[1] = GroundHitsLeft;
-         AllRaycastHits[2] = GroundHitsRight;
+        AllRaycastHits[0] = GroundHitsCenter;
+        AllRaycastHits[1] = GroundHitsLeft;
+        AllRaycastHits[2] = GroundHitsRight;
 
-        WallRayPositionLeft = transform.position + new Vector3(-RayPositionOffset, .5f, 0);
-        WallRayPositionRight = transform.position + new Vector3(RayPositionOffset, .5f, 0);
+        OnGround = RayListCheck(AllRaycastHits);
+        CanJump = OnGround;
+
+        //Wall Jump/Slide Ray Setup
+        WallRayPositionLeft = transform.position + new Vector3(-RayPositionOffset, -.5f, 0);
+        WallRayPositionRight = transform.position + new Vector3(RayPositionOffset, -.5f, 0);
 
         WallHitsLeft = Physics2D.RaycastAll(WallRayPositionLeft, Vector2.left, RayLength);
-        WallHitsRight = Physics2D.RaycastAll(WallRayPositionLeft, -Vector2.left, RayLength);
+        WallHitsRight = Physics2D.RaycastAll(WallRayPositionRight, -Vector2.left, RayLength);
 
-        //OnWallLeft + RayCheck(WallHitsLeft);
-        //OnWallRight + RayCheck(WallHitsRight);
+        OnWallLeft = RayCheck(WallHitsLeft);
+        OnWallRight = RayCheck(WallHitsRight);
 
-        DrawRay();
+        DrawRays();
     }
 
     private bool RayCheck(RaycastHit2D[] RayHits)
@@ -86,38 +110,57 @@ public class PlayerMovement : MonoBehaviour
                     return true;
                 }
             }
-             
+
         }
-        
         return false;
     }
-
 
     private void Jump()
     {
         RaySetup();
 
-        CanJump = RayListCheck(AllRaycastHits);
+        //Ground Jump
         if (Input.GetKey(KeyCode.Space) && CanJump)
         {
-         rb.velocity = new Vector2(rb.velocity.x, 0);
-         rb.velocity = new Vector2(rb.velocity.x, JumpSpeed);
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.velocity = new Vector2(rb.velocity.x, JumpSpeed);
         }
 
         if (!OnGround)
         {
+            //Wall Slide
             if (CanMove)
             {
                 if (Input.GetAxisRaw("Horizontal") < 0 && OnWallLeft)
                 {
                     rb.velocity = new Vector2(rb.velocity.x, -WallSlideSpeed);
                 }
-                else if (Input.GetAxisRaw("Horizontal") > 0 && OnWallRight) 
+                else if (Input.GetAxisRaw("Horizontal") > 0 && OnWallRight)
                 {
                     rb.velocity = new Vector2(rb.velocity.x, -WallSlideSpeed);
                 }
             }
+
+            //Wall Jump
+            if (Input.GetKeyDown(KeyCode.Space) && OnWallLeft)
+            {
+                rb.velocity = new Vector2(JumpSpeed * 0.5f, JumpSpeed);
+                StartCoroutine(WallJumpCoolDown());
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space) && OnWallRight)
+            {
+                rb.velocity = new Vector2(-JumpSpeed * 0.5f, JumpSpeed);
+                StartCoroutine(WallJumpCoolDown());
+            }
         }
+    }
+
+    IEnumerator WallJumpCoolDown()
+    {
+        CanMove = false;
+        yield return new WaitForSeconds(0.25f);
+        CanMove = true;
     }
 
     private bool RayListCheck(RaycastHit2D[][] RayHits)
@@ -128,95 +171,29 @@ public class PlayerMovement : MonoBehaviour
             {
                 if (hit.collider != null)
                 {
-                    if(hit.collider.tag != "PlayerCollider")
+                    if (hit.collider.tag != "PlayerCollider")
                     {
                         return true;
                     }
-                    
                 }
             }
         }
         return false;
     }
 
-
-    private void Movement()
+    private void DrawRays()
     {
-        if( Input.GetAxisRaw("Horizontal") > 0)
-        {
-            rb.velocity = new Vector2(MovementSpeed * Time.fixedDeltaTime, rb.velocity.y);
-        }
-        else if (Input.GetAxisRaw("Horizontal") < 0)
-        {
-            rb.velocity = new Vector2(-MovementSpeed * Time.fixedDeltaTime, rb.velocity.y);
-            
-        }
-        else
-        {
-            rb.velocity = new Vector2(0, rb.velocity.y);
-        }
-    }
-
-    private void DrawRay()
-    {
+        //Ground Rays
         Debug.DrawRay(RayPositionCenter, -Vector2.up * RayLength, Color.red);
         Debug.DrawRay(RayPositionLeft, -Vector2.up * RayLength, Color.red);
         Debug.DrawRay(RayPositionRight, -Vector2.up * RayLength, Color.red);
 
+        //Wall Rays
         Debug.DrawRay(WallRayPositionLeft, Vector2.left * RayLength, Color.blue);
-        Debug.DrawRay(WallRayPositionRight, Vector2.left * RayLength, Color.blue);
-
+        Debug.DrawRay(WallRayPositionRight, -Vector2.left * RayLength, Color.blue);
     }
 
-
-
-
-
 }
-//im leaving this here because no one will ever find it
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
