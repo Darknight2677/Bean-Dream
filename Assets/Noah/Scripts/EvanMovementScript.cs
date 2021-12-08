41cbd937d477620543bedf81ce67dddb8bee024a
MUKILTEO\1709575 <noahb2675@gmail.com>
2021-12-08 10:04:07 -0800

    added stuff to timer

--- Assets/Noah/Scripts/EvanMovementScript.cs
+++ Assets/Noah/Scripts/EvanMovementScript.cs
@@ -6,7 +6,6 @@ public class EvanMovementScript : MonoBehaviour
 {
     bool grounded = false;
     Rigidbody2D rb2;
-    //SpriteRenderer sr;
     Animator a;
     public GameObject spawnPoint;
     public GameObject mainPlayer;
@@ -15,59 +14,26 @@ public class EvanMovementScript : MonoBehaviour
     public int health;
     public HealthBar healthBar;
 
+    public Timer timer;
+
     // Start is called before the first frame update
     void Start()
     {
+
         rb2 = gameObject.GetComponent<Rigidbody2D>();
-        //sr = gameObject.GetComponent<SpriteRenderer>();
         a = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //HEAD
-
-        //HEAD
-        //a.SetFloat("yVelocity", rb2.velocity.y);
-        //a.SetBool("Grounded", grounded);
-
-        float horizValue = Input.GetAxis("Horizontal");
-
-        //if (horizValue == 0)
-        //{
-            //a.SetBool("Moving", false);
-        //}
-
-        //if (horizValue > 0)
-        //{
-            //a.SetBool("Moving", true);
-        //}
-
-        //if (horizValue < 0)
-        //{
-            //a.SetBool("Moving", true);
-        //}
-
-        rb2.velocity = new Vector2(horizValue * 2, rb2.velocity.y);
-
-        if (horizValue > 0)
-        {
-            //sr.flipX = false;
-        }
-
-        if (horizValue < 0)
-        {
-            //sr.flipX = true;
-        }
-
+    
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
             health--;
-            //Destroy(collision.gameObject);
             if (health < 1)
             {
                 healthBar.SetHealth(maxHealth);
