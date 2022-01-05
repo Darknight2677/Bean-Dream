8976f844a7ce2b26fe45f7a990608bdf8c1bdb39
Brenden <1711654@mukilteo.wednet.edu>
2022-01-05 10:13:53 -0800

    added falling blocks

---
+++ Assets/Brenden/Scripts/Brick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		rb.isKinematic = true;
+	}
+
+	void OnCollisionEnter2D (Collision2D col)
+	{
+		Debug.Log(col.gameObject.name);
+		if (col.gameObject.name.Equals ("Player")) {
+			Invoke ("DropPlatform", 0.5f);
+			Destroy (gameObject, 2f);
+		}
+	}
+
+	void DropPlatform()
+	{
+		rb.isKinematic = false;
+	}
+}
